dd20a0de1ac9eca6cbb16e4a2dc69639ba359df4
josie-woodman <59493526+josie-woodman@users.noreply.github.com>
2021-01-23 20:58:47 -0500

    Add files via upload

---
+++ woodman_CEIL865_Lab1/woodman_CEIL865Lab1/Convert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace woodman_CEIL865Lab1
+{
+    public class Convert
+    {
+         //<System.Web.Services.WebMethod()> _
+       // Public Function FahrenheitToCelsius(ByVal Fahrenheit As Double) _
+     //  // As Double
+     //   Return((Fahrenheit - 32) * 5) / 9
+  //  End Function
+
+  //  <System.Web.Services.WebMethod()> _
+  //      Public Function CelsiusToFahrenheit(ByVal Celsius As Double) _
+  //          As Double
+  //      Return((Celsius* 9) / 5) + 32
+  //  End Function
+  //  }
+}
